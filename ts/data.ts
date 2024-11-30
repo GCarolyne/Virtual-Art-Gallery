ff67d8ebb34770867f5ed30d75aae9761d866f7a
Carolyne Golias <goliascarolyne@gmail.com>
2024-11-30 23:11:48 +0000

    updated handleinput function

--- ts/data.ts
+++ ts/data.ts
@@ -1,45 +1,63 @@
-/* exported data */
-// interface ImgData {
-//   artistTitle: string;
-//   artworkTypeTitle: string;
-//   imageId: string;
+// /* exported data */
+//  interface DataModel {
+//       view: string;
+//       results: myObjectImgs[],
+//       nextEntryId: 1,
+//       entryId: 1,
 // }
-// console.log('ImgData',ImgData)
+//  console.log('DataModel')
 
-async function fetchArtObjects(): Promise<void> {
-  try {
-    const response = await fetch('https://api.artic.edu/api/v1/artworks');
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} ${response.statusText}`);
-    }
+// async function fetchArtObjects(): Promise<void> {
+//   try {
+//     const response = await fetch('https://api.artic.edu/api/v1/artworks/');
 
-    const data = await response.json();
-    console.log(data);
-    // const imageUrl = artData.config.iiif_url;
-    // const appendD = '/full/843,/0/default.jpg';
+//     if (!response.ok) {
+//       throw new Error(`Error: ${response.status} ${response.statusText}`);
+//     }
+//     const artData = await response.json()
+//     const imageUrl = artData.config.iiif_url;
+//     const appendD = '/full/843,/0/default.jpg';
+//     for (let i = 0; i < artData.data.length; i++) {
+//       const currentArtWork = artData.data[i];
+//       const url = `${imageUrl}${currentArtWork.image_id}${appendD}`;
+//       const $image = document.createElement('img');
+//       $image.setAttribute('src', url);
+//       console.log('$image',$image)
 
-    // for (let i = 0; i < artData.length; i++) {
-    //   const currentArtWork = artData[i];
-    //   const url = `${imageUrl}${currentArtWork.image_id}${appendD}`;
-    //   const $image = document.createElement('img');
-    //   $image.setAttribute('src', url);
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-}
-fetchArtObjects();
+//   } catch (error) {
+//     console.error('An error occurred:', error);
+//   }
+//   }
+// }
+// fetchArtObjects();
 
-async function fetchPoetObjects(): Promise<void> {
-  try {
-    const response = await fetch('https://poetrydb.org/title/Sonnet');
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} ${response.statusText}`);
-    }
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-}
+// const data = readData()
 
-fetchPoetObjects();
+// function readData (): DataModel {
+//   const storedDataJSON = localStorage.getItem('data')
+//   if (storedDataJSON) {
+//     return JSON.parse (storedDataJSON);
+//   } else {
+//     return {
+//       view: 'search-results',
+//       results: [],
+//       nextEntryId: 1,
+//       entryId: 1,
+//     }
+//   }
+// }
+
+// async function fetchPoetObjects(): Promise<void> {
+//   try {
+//     const response = await fetch('https://poetrydb.org/title/Sonnet');
+//     if (!response.ok) {
+//       throw new Error(`Error: ${response.status} ${response.statusText}`);
+//     }
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error('An error occurred:', error);
+//   }
+// }
+
+// fetchPoetObjects();
--- ts/main.ts
+++ ts/main.ts
@@ -11,23 +11,52 @@
 //   imageId: string;
 // }
 
-// query for all the dom objects in html to create them here in TS
-// working with search bar input data
+// // query for all the dom objects in html to create them here in TS
+// // working with search bar input data
 
-//
+// const $resultsSearch = document.querySelector('search-results');
+// if (!$resultsSearch) throw new Error('the $resultsSearch query failed.');
+
+// const $mainPage = document.querySelector('#search-page');
+// if (!$mainPage) throw new Error('the $mainPage query failed.');
 
 // const $searchForm = document.querySelector('form');
 // if (!$searchForm) throw new Error('the $searchForm query failed.');
 
-// function submitForm(event: Event): undefined {
-//   event.preventDefault();
-//   if (!$searchForm) throw new Error('the $searchForm query failed.');
-//   const $ImgData = $searchForm.elements as FormElement;
-//   console.log($ImgData);
-//   const myObjectImg:myObjectImgs = {
-//     artistTitle: $ImgData.artist_artist_titles
-//   }
-// image_id
-// artist_titles
-// artwork_type_title
+// const $imageObject = document.querySelector('img');
+// if (!$imageObject) throw new Error('the $imageObject query failed.');
+
+// const $userInput = document.querySelector('user-input');
+// if (!$userInput) throw new Error(' the $userInput query failed.');
+
+// console.log('$imageObejct', $imageObject);
+
+// function handleInput(event: Event): void {
+//   if (!$userInput) throw new Error(' the $userInput query failed.');
+//   const $eventTarget = event.target as HTMLInputElement;
+//   const eventTargetValue = $eventTarget.value;
+//   $userInput.setAttribute('src', eventTargetValue);
 // }
+
+// $resultsSearch.addEventListener('input', handleInput);
+
+// function viewSwap(viewName: string): void {
+//   if (!$resultsSearch) throw new Error('the $resultsSearch query failed.');
+//   if (!$mainPage) throw new Error('the $mainPage query failed.');
+//   if (viewName === 'search-page') {
+//     $resultsSearch.classList.add('hidden');
+//     $mainPage.classList.remove('hidden');
+//   } else if (viewName === 'search-results') {
+//     $resultsSearch.classList.remove('hidden');
+//     $mainPage.classList.add('hidden');
+//   }
+// }
+// viewSwap();
+
+// function submitForm(event: Event): undefined {
+//   if (!$searchForm) throw new Error('the $searchForm query failed.');
+//   const $searchInput = $searchForm.elements as FormElement;
+//   console.log('$searchInput', $searchInput);
+//   // event.preventDefault();
+// }
+// submitForm();
